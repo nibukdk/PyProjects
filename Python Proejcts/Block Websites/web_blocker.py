f4f750844a4a275171f0d0b01bf162e32df26d14
Nibesh Khadka <nibeshkhadka@gmail.com>
2018-11-10 22:53:37 +0200

    File is ready to run

--- Python Proejcts/Block Websites/web_blocker.py
+++ Python Proejcts/Block Websites/web_blocker.py
@@ -2,23 +2,31 @@
 import time
 from datetime import datetime as dt
 
-host_temp = "hosts"
-host_path = r"C:\WINDOWS\system32\drivers\etc"
+# Hosts file path for windows 10
+host_path = r"C:\WINDOWS\system32\drivers\etc\hosts"
 redirect = "127.0.0.1"
-
-websites = ["www.facebook.com"]
+# Websites list you want to restrict
+websites = ["www.facebook.com"
+            ]
 
 while True:
     # if time is betwen 08-18, and 20:06
     if (dt(dt.now().year, dt.now().month, dt.now().day, 00) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 6) or dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 18) or dt(dt.now().year, dt.now().month, dt.now().day, 20) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 23, 59, 59)):
-        with open("hosts", 'r+') as file:
+        with open(host_path, 'r+') as file:
             content = file.read()
             for website in websites:
                 if website in content:
                     pass
                 else:
                     file.write(redirect + " " + website + "\n")
+                    print("This is work hour.")
     else:
-        print("Now you can open")
-
+        with open(host_path, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in websites):
+                    file.write(line)
+            file.truncate()
+            print("This is fun hour")
     time.sleep(5)
